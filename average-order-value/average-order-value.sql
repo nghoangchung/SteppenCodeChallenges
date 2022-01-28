ea72001e485de41803acb8c67a36be405ea1bfa2
rikai-chungnguyen <chung.nguyen@rikai.technology>
2022-01-28 12:39:40 +0700

    test 4 done

---
+++ average-order-value/average-order-value.sql
@@ -0,0 +1,13 @@
+select customers.name,
+    count(DISTINCT order_line_items.order_id) as num_orders,
+    SUM((order_line_items.unit_price * order_line_items.quantity)) as total_order_value,
+    (SUM((order_line_items.unit_price * order_line_items.quantity)) / count(orders.customer_id)) as avg_order_value
+from orders 
+INNER JOIN customers
+        on orders.customer_id = customers.customer_id
+INNER JOIN order_line_items
+        on orders.order_id = order_line_items.order_id
+GROUP BY customers.name            
+ORDER BY avg_order_value DESC
+
+--  i don't know how to calculate avg --
