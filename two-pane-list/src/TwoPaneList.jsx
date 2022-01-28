468c765ffc1564e9fc2c4919acf00137104ab802
rikai-chungnguyen <chung.nguyen@rikai.technology>
2022-01-28 14:01:48 +0700

    test 5 done

--- two-pane-list/src/TwoPaneList.jsx
+++ two-pane-list/src/TwoPaneList.jsx
@@ -1,3 +1,23 @@
 export const TwoPaneList = ({ data }) => {
-  return null
-}
+  return (
+    // this is a first time i'm used bulma :(
+    // i spend too much time to di test 5, but not good and wrong. i miss to dislay content  
+    <div className="columns">
+      <aside class="menu">
+        <ul class="menu-list">
+          {data.map((e, i) => (
+            <li key={i} >
+                  <a class="is-active">{e.title}</a>
+                  <ul>
+                    {e.content.map((val, index) => (
+                      <li key={index}>{val}</li>
+                    ))}
+                  </ul>
+              
+            </li>
+          ))}
+        </ul>
+      </aside>
+    </div>
+  );
+};
