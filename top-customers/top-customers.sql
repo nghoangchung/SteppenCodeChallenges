41c4d664dc6c4cf98e23a893153ac07e344fbb18
rikai-chungnguyen <chung.nguyen@rikai.technology>
2022-01-29 13:12:11 +0700

    test 6 done

---
+++ top-customers/top-customers.sql
@@ -0,0 +1,14 @@
+SELECT
+    CAST(SUBSTR(ordered_at, 1, 4) AS integer) as year,
+    CAST(SUBSTR(ordered_at, 6, 7) AS integer) as month,
+    customer_id,
+--    unit_price,
+--    quantity,
+    SUM((unit_price * quantity)) as total_monthly_order_value
+FROM
+    order_line_items
+    INNER JOIN orders ON order_line_items.order_id = orders.order_id
+GROUP BY
+    customer_id
+
+-- It not a perfect answer, but i try my best    
